bbc71359b5d4586a00b5d9ab7f2573188847259e
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-03-08 03:47:34 -0800

    completed model + bug fix
    Exercise Model
    - exerciseSchema
    - createExercise
    - findExercise
    - findExerciseById
    - updateExercise
    - deleteExercise
    
    Bug Fix
    - typo in package.json keeping nodemon from working correctly

--- exercises-rest/exercises_model.mjs
+++ exercises-rest/exercises_model.mjs
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import "dovenv/config";
+import "dotenv/config";
 
 mongoose.connect(
     process.env.MONGODB_CONNECT_STRING,
@@ -11,18 +11,65 @@ db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
-const exerciseShema = mongoose.Schema();
+const exerciseShema = mongoose.Schema({
+    name: { type: String, required: true },
+    reps: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    unit: { type: String, required: true },
+    date: { type: String, required: true }
+});
 
 const Exercise = mongoose.model("Exercise", exerciseShema);
 
-const createExercise = async();
+/**
+ * @param {String} unit "kgs" or "lbs"
+ * @param {String} date "MM-DD-YY" e.g. "07-30-21"
+ */
+const createExercise = async (name, reps, weight, unit, date) => {
+    const exercises = Exercise({
+        name: name,
+        reps: reps,
+        weight: weight,
+        unit: unit,
+        date: date
+    });
+    return exercises.save();
+};
 
-const findExercises = async();
+const findExercises = async (filter, projection, limit) => {
+    const query = Exercise.find(filter)
+        .select(projection)
+        .limit(limit);
+    return query.exec();
+};
 
-const findExerciseById = async();
+const findExerciseById = async (_id) => {
+    const query = Exercise.findById(_id);
+    return query.exec();
+};
 
-const updateExercise = async();
+/**
+ * @param {String} unit "kgs" or "lbs"
+ * @param {String} date "MM-DD-YY" e.g. "07-30-21"
+ */
+const updateExercise = async (_id, name, reps, weight, unit, date) => {
+    const result = await Exercise.replaceOne({
+        _id: _id
+    },
+        {
+            name: name,
+            reps: reps,
+            weight: weight,
+            unit: unit,
+            date: date
+        }
+    );
+    return result.modifiedCount;
+};
 
-const deleteExercise = async();
+const deleteExercise = async (_id) => {
+    const result = await Exercise.deleteOne({ _id: _id });
+    return result.deletedCount;
+};
 
 export { createExercise, findExercises, findExerciseById, updateExercise, deleteExercise };
