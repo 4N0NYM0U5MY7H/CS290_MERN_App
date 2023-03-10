8f37790ce63c733f538d5aaad4cbf2d4ec1f36ff
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-03-09 19:20:39 -0800

    Home Page
    - rendered on app startup
    - calls `GET /exercises` REST API endpoint
    - displays data in HTML table
    - ID value is not shown
    - Includes 2 react icons
      - delete icon calls `DELETE /exercises/:_id` REST API endpoint
      - edit icon navigates to Edit Exercise Page

--- exercises-ui/src/pages/HomePage.js
+++ exercises-ui/src/pages/HomePage.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ExerciseList from "../components/ExerciseList";
 
-export default function HomePage() {
+export default function HomePage({ setExerciseToEdit }) {
+    const [exercises, setExercises] = useState();
+    const navigate = useNavigate();
+
+    const onDelete = async _id => {
+        const response = await fetch(`/exercises/${_id}`, { method: "DELETE" });
+        if (response.status === 204) {
+            setExercises(exercises.filter(ex => ex._id !== _id));
+        } else {
+            console.error(`Failed to delete exercise _id: ${_id}, status code: ${response.status}`);
+        }
+    };
+
+    const onEdit = exercise => {
+        setExerciseToEdit(exercise);
+        navigate("/edit-exercise");
+    };
+
+    const loadExercises = async () => {
+        const response = await fetch("/exercises");
+        const exercises = await response.json();
+        setExercises(exercises);
+    };
+
+    useEffect(() => {
+        loadExercises();
+    }, []);
 
     return (
-        <>
+        <main>
             <h2>Home Page</h2>
-        </>
+            <ExerciseList exercises={exercises} onDelete={onDelete} onEdit={onEdit}></ExerciseList>
+        </main>
     );
-
 };
