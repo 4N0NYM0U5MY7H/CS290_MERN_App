1be008cf0cf355949902c59d7dc3a3e78e81b431
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-03-09 19:58:39 -0800

    Update EditExercise.js
    - edit specified exercise based on edit icon
    - pre-populated row data
    - Save button calls `PUT /exercises/:_id` REST API endpoint
      - On success: response status `200` w/alert + navigate to Home page
      - On fail: response status w/alert + navigate to Home Page

--- exercises-ui/src/pages/EditExercise.js
+++ exercises-ui/src/pages/EditExercise.js
@@ -1,12 +1,85 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function EditExercisePage() {
+export default function EditExercisePage({ exerciseToEdit }) {
+
+    const [name, setName] = useState(exerciseToEdit.name);
+    const [reps, setReps] = useState(exerciseToEdit.reps);
+    const [weight, setWeight] = useState(exerciseToEdit.weight);
+    const [unit, setUnit] = useState(exerciseToEdit.unit);
+    const [date, setDate] = useState(exerciseToEdit.date);
+
+    const navigate = useNavigate();
+
+    const editExercise = async () => {
+        const editExercise = { name, reps, weight, unit, date };
+        const response = await fetch(`/exercises/${exerciseToEdit._id}`, {
+            method: "PUT",
+            body: JSON.stringify(editExercise),
+            headers: {
+                "Content-Type": "aplication/json",
+            },
+        });
+        if (response.status === 200) {
+            alert("Successfully edited the exercise");
+        } else {
+            alert(`Failed to edit exercise. Status Code: ${response.status}`);
+        }
+        navigate("/");
+    };
 
     return (
-        <>
-            <h2>Edit Exercise Page</h2>
-        </>
+        <main>
+            <h2>Edit Exercise</h2>
+            <input
+                name="name"
+                type="text"
+                required
+                pattern="(?!^$)([^\s])"
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <span className="validity"></span>
+            <input
+                name="reps"
+                type="number"
+                required
+                pattern="^0*[1-9][0-9]*"
+                value={name}
+                onChange={e => setReps(e.target.value)}
+            />
+            <span className="validity"></span>
+            <input
+                name="weight"
+                type="number"
+                required
+                pattern="^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$"
+                value={weight}
+                onChange={e => setWeight(e.target.value)}
+            />
+            <span className="validity"></span>
+            <select
+                name="unit"
+                required
+                value={unit}
+                onChange={e => setUnit(e.target.value)}
+            >
+                <option value="kgs">kgs</option>
+                <option value="lbs">lbs</option>
+            </select>
+            <input
+                name="date"
+                type="text"
+                required
+                pattern="^(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])(-)\d{2}$"
+                value={date}
+                onChange={e => setDate(e.target.value)}
+            />
+            <span className="validity"></span>
+            <button
+                onClick={editExercise}>
+                Save
+            </button>
+        </main>
     );
-
 };
